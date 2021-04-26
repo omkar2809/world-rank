f7cc31adbd1e5d16d18e4f8731132a7a8e265b42
omkar2809 <zemseom10@gmail.com>
2021-04-26 19:00:22 +0530

    layout and Search Input

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
-import Head from 'next/head'
+import Layout from '../components/Layout/Layout'
+import SearchInput from '../components/SearchInput/SearchInput'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        main
-      </main>
-
-      <footer className={styles.footer}>
-        footer
-      </footer>
+export default function Home({ countries }) {
+  console.log(countries)
+  return <Layout>
+    <div className={styles.count}>
+      Found {countries.length} countries
+      <SearchInput placeholder="Filter By Name, Region or Sub-Region" />
     </div>
-  )
+  </Layout>
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all')
+  const countries = await res.json() 
+  return {
+    props: {
+      countries
+    }
+  }
 }
