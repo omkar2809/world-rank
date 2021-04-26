b0520d7f4dcaf70c99b66f1fbefeafde7f4b96af
omkar2809 <zemseom10@gmail.com>
2021-04-26 20:47:01 +0530

    Countries Table and Searching

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,33 @@
+import { useState } from 'react'
+import CountriesTable from '../components/CountriesTable/CountriesTable'
 import Layout from '../components/Layout/Layout'
 import SearchInput from '../components/SearchInput/SearchInput'
 import styles from '../styles/Home.module.css'
 
 export default function Home({ countries }) {
-  console.log(countries)
-  return <Layout>
-    <div className={styles.count}>
-      Found {countries.length} countries
-      <SearchInput placeholder="Filter By Name, Region or Sub-Region" />
-    </div>
-  </Layout>
+  const [keyword, setKeyword] = useState('')
+
+  const filteredCountries =  countries.filter(country => 
+    country.name.toLowerCase().includes(keyword) || 
+    country.region.toLowerCase().includes(keyword) ||
+    country.subregion.toLowerCase().includes(keyword)
+  )
+
+  const onInputChange = e => {
+    e.preventDefault()
+    setKeyword(e.target.value.toLowerCase())
+  }
+
+  return (
+    <Layout>
+      <div className={styles.count}>
+        Found {filteredCountries.length} countries
+      </div>
+      <SearchInput placeholder="Filter By Name, Region or Sub-Region" onChange={onInputChange} />
+
+      <CountriesTable countries={filteredCountries}/>
+    </Layout>
+  )
 }
 
 export const getStaticProps = async () => {
